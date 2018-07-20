cba92bd9d79fe39b6e41c1b9b3dd8a4eac8f9229
May Chen <maychen@Mays-MacBook-Pro.local>
2018-07-20 01:11:57 -1000

    Working on getting 'get' method to work

--- linkedList.js
+++ linkedList.js
@@ -8,6 +8,8 @@ function linkedListGenerator(){
     //global variables
     let head = null;    //first object in list
     let tail = null;    //last object in list
+    let length = 0;
+    let currentNode = null;
 
     //A node has a data value and the next node it points to
     class Node{
@@ -30,25 +32,43 @@ function linkedListGenerator(){
     add = (value) => {
         //create a new node containing a value and next pointer
         const myNode = new Node;
+        length++;   //increase length of list
 
         //If empty list (no nodes yet) and head points to null; head and tails should point to same first node
         if(getHead() === null){
-            head = myNode;
-            tail = myNode;
             myNode.value = value;
             myNode.next = null;
+            head = myNode;
+            tail = myNode;
+            currentNode = myNode;
             return myNode;
         }
         else{
             myNode.value = value;
             myNode.next = null;
+            currentNode = myNode;
+            console.log("currentNode: ", currentNode);
             tail = myNode;
             return myNode;
         }
 
     };
 
-    get = () => {};
+    get = (number) => {
+        console.log("at get, head: ", getHead());
+        currentNode = getHead();
+        console.log("at get, currentNode: ", currentNode);
+        if(number === length){
+            return currentNode;
+        }
+        else{
+            currentNode = currentNode.next;
+            console.log("here: " ,currentNode)
+            length--;
+        }
+
+
+    };
     remove = () => {};
     insert = () => {};
 
