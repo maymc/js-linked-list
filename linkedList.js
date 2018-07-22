f28447bb5fffab9456711ee5a167175395db3fab
May Chen <maychen@Mays-MacBook-Pro.local>
2018-07-21 18:32:58 -1000

    Refactored code a little to make it cleaner

--- linkedList.js
+++ linkedList.js
@@ -8,7 +8,6 @@ function linkedListGenerator() {
   let head = null; //first object in list
   let tail = null; //last object in list
   let length = 0;
-  let currentNode = null;
 
   //A node has a data value and the next node it points to
   class Node {
@@ -18,22 +17,30 @@ function linkedListGenerator() {
     }
   }
 
+  //Return value of first node of the list
   const getHead = () => {
     console.log("getHead: ", head);
     return head;
   };
 
+  //Return value of last node of the list
   const getTail = () => {
     console.log("getTail: ", tail);
     return tail;
   };
 
+  //Take in any data value and add a new node to the end of a list. Return the new node that was created
   const add = value => {
     console.log("ENTERED ADD METHOD");
-    //create a new node containing a value and next pointer
+    //Create a new instance of node
     const myNode = new Node();
 
-    //If empty list (no nodes yet) and head points to null; head and tails should point to same first node
+    const setUpNode = value => {
+      myNode.value = value;
+      myNode.next = null;
+    };
+
+    //If empty list (no nodes yet) and head points to null; head and tails should point to same first node added to the list
     console.log("Head at ADD: ", head);
     console.log("Tail at ADD: ", tail);
 
@@ -44,30 +51,38 @@ function linkedListGenerator() {
       length++; //increase length of list
       console.log("length: " + length);
 
-      myNode.value = value;
-      myNode.next = null;
+      //Assign the value and next pointer to the new node
+      setUpNode(value);
       console.log("myNode: ", myNode);
 
       head = myNode;
       tail = myNode;
+
+      //DEBUG - Check the new list/head/tail
       console.log("Head after 'if': ", head);
       console.log("Tail after 'if': ", tail);
       console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
       return myNode;
-    } else {
+    }
+    //Else, list is not empty, add new nodes to the end of the list
+    else {
       console.log("Adding new node at ELSE");
       console.log("Entering ELSE statement b/c head != null");
 
       length++; //increase length of list
       console.log("length: " + length);
 
-      myNode.value = value;
-      myNode.next = null;
+      //Assign the value and next pointer to the new node
+      setUpNode(value);
+
+      //Previous tail's next should point to new node that is added to the end of the list. New node should be the tail.
       console.log("myNode: ", myNode);
       tail.next = myNode;
       console.log("Previous Next is pointing at: ", tail.next);
       tail = myNode;
+
+      //DEBUG - Check the list/head/tail
       console.log("Head after 'else': ", head);
       console.log("Tail after 'else': ", tail);
       console.log("END ADD METHOD");
@@ -82,7 +97,7 @@ function linkedListGenerator() {
     console.log("GET - number: ", number);
     console.log("GET - head: ", head);
     console.log("GET - tail: ", tail);
-    console.log("GET - length: " + length);
+    console.log("GET - list length: " + length);
 
     //If the number is 0, head is the node
     if (number === 0) {
@@ -108,7 +123,7 @@ function linkedListGenerator() {
         console.log("nextNode: ", nextNode);
         return nextNode;
       }
-      //Else if the number is referrig to a node between the node after head and the tail
+      //Else if the number is referring to a node between the node after head and the tail
       else if (number !== 1 && number < length && number > 0) {
         console.log("tempLength before: ", tempLength);
 
@@ -141,9 +156,7 @@ function linkedListGenerator() {
       console.log("Removing head, this is head: ", head);
       console.log("This is next node after head: ", head.next);
       head = head.next;
-    }
-
-    if (number > 0 && number < length) {
+    } else if (number > 0 && number < length) {
       //Set the previous node's next to point to the node to be removed's next. The node to remove will disappear after there is no reference to it
       let currentNode = get(number);
       console.log("currentNode's next: ", currentNode.next);
@@ -165,8 +178,8 @@ function linkedListGenerator() {
       length--;
       console.log("new length: ", length);
 
+      //DEBUG - Check the tail
       console.log("current tail: ", tail);
-
       console.log("END REMOVE METHOD");
     } else {
       console.log("NUMBER DOESN'T EXIST. END REMOVE METHOD");
@@ -178,31 +191,82 @@ function linkedListGenerator() {
     console.log("ENTERED INSERT METHOD");
     console.log("value is: " + value + ", insert position is: ", number);
     console.log("Length at insert: ", length);
+    let nodeToMoveOver = null;
+    let nodeBeforeInserted = null;
+    let nodeToInsert = null;
+    let previousTail = null;
 
-    // //If adding to the end, number should be length+1; Add a node as usual
-    // if (number === length + 1) {
-    //   add(value); //gets added to the end
-    //   console.log("This is the added node: ", get(number - 1));
-    // }
-    // //Else if adding it to the front, reassign head and head.next
-    // else {
-    //   console.log("Current node at insert position: ", get(number));
-    //   add(value);
-    // }
+    const reassignPointers = () => {
+      nodeToInsert = get(length - 1);
+      nodeToInsert.next = nodeToMoveOver;
+    };
 
     //If the number is greater than the list's length
     if (number > length || number < 0) {
       console.log("END INSERT METHOD");
       return false;
+    } else if (number >= 0 && number <= length) {
+      console.log("Number passed, will insert new node somewhere!");
+      console.log("Current node at desired insert position: ", get(number));
+
+      //If the nodeToMoveOver is actually the tail, the tail will move when a new node is added. Set tail flag to true to indicate resetting the tail after new node is added.
+      if (get(number) === tail) {
+        nodeToMoveOver = tail;
+        console.log("nodeToMoveOver: ", nodeToMoveOver);
+
+        nodeBeforeInserted = get(number - 1);
+        console.log("nodeBeforeInserted: ", nodeBeforeInserted);
+
+        //When node is added, it goes to the end, get the node
+        add(value);
+        reassignPointers();
+        nodeBeforeInserted.next = nodeToInsert;
+        nodeToMoveOver.next = null;
+
+        tail = nodeToMoveOver; //Reassign the tail
+      }
+      //If nodeToMoveOver is actually the head, there is no node before it. Set the headFlag to true to indicate resetting the head after the new node is added
+      else if (get(number) === head) {
+        nodeToMoveOver = head;
+        console.log("nodeToMoveOver: ", nodeToMoveOver);
+
+        //Save the current tail before it is reassigned
+        previousTail = tail;
+        console.log("Saved previousTail: ", previousTail);
+
+        //When node is added, it goes to the end, get the node
+        add(value);
+        reassignPointers();
+        head = nodeToInsert;
+        tail = previousTail;
+        tail.next = null;
+      } else {
+        nodeToMoveOver = get(number);
+        console.log("nodeToMoveOver: ", nodeToMoveOver);
+
+        nodeBeforeInserted = get(number - 1);
+        console.log("nodeBeforeInserted: ", nodeBeforeInserted);
+
+        previousTail = tail;
+
+        add(value);
+        reassignPointers();
+        nodeBeforeInserted.next = nodeToInsert;
+        tail = previousTail;
+        tail.next = null;
+      }
+
+      console.log("CHECK LIST: ", head);
+      console.log("END INSERT METHOD");
     }
   };
 
   return {
-    getHead: getHead,
-    getTail: getTail,
-    add: add,
-    get: get,
-    remove: remove,
-    insert: insert
+    getHead,
+    getTail,
+    add,
+    get,
+    remove,
+    insert
   };
 }
